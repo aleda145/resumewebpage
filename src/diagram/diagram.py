cc62d833ba2ff317b8e589601f979fb1d39d743c
Alexander Dahl <alex@dahl.dev>
2021-03-24 21:19:17 +0100

    Cont work on diagram

--- src/diagram/diagram.py
+++ src/diagram/diagram.py
@@ -1,5 +1,5 @@
 # diagram.py
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
@@ -10,14 +10,32 @@ from diagrams.onprem.network import Traefik
 from diagrams.onprem.vcs import Github
 
 from diagrams.programming.framework import Angular
+from diagrams.onprem.workflow import Airflow
 
+from diagrams.elastic.elasticsearch import Elasticsearch, Beats, Kibana, LogStash
 
 from diagrams.custom import Custom
 
-
-with Diagram("Web Pipeline", show=False):
-    watchtower = Custom("WatchTower", "./logos/watchtower.png")
+with Diagram("Home Server", show=False):
+    github = Github("github")
     dockerhub = Custom("dockerhub", "./logos/dockerhub.png")
+    with Cluster("Docker"):
+        watchtower = Custom("WatchTower", "./logos/watchtower.png")
 
-    Angular("web") >> Github("github") >> Droneci("drone") >> dockerhub >> watchtower
-
+        logstash = LogStash("Logstash")
+        elastic = Elasticsearch("Elasticsearch")
+        logstash >> elastic
+        with Cluster("Traefik"):
+            kibana = Kibana("Kibana")
+            angular = Angular("web")
+            drone = Droneci("drone")
+            angular >> github >> drone >> dockerhub >> watchtower
+            airflow = Airflow("")
+            airflow >> logstash
+            elastic >> kibana
+        traefik = Traefik()
+        traefik >> logstash
+        traefik >> angular
+        traefik >> drone
+        traefik >> kibana
+        traefik >> airflow
