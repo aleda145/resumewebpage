a1fe5f1a2ae0a671b4c3f69be0805f4bd3fd6377
Alexander Dahl <alex@dahl.dev>
2021-03-25 13:18:00 +0100

    Add diagram as code

--- src/diagram/diagram.py
+++ src/diagram/diagram.py
@@ -1,5 +1,5 @@
 # diagram.py
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
@@ -16,26 +16,50 @@ from diagrams.elastic.elasticsearch import Elasticsearch, Beats, Kibana, LogStas
 
 from diagrams.custom import Custom
 
-with Diagram("Home Server", show=False):
-    github = Github("github")
-    dockerhub = Custom("dockerhub", "./logos/dockerhub.png")
+with Diagram("Home Server Architecture", show=False):
+    github = Github("Github")
+    dockerhub = Custom("", "./logos/dockerhub.png")
+    debian = Custom("Debian logs", "./logos/debian.png")
     with Cluster("Docker"):
         watchtower = Custom("WatchTower", "./logos/watchtower.png")
-
         logstash = LogStash("Logstash")
+        beats = Beats("Filebeat")
         elastic = Elasticsearch("Elasticsearch")
         logstash >> elastic
-        with Cluster("Traefik"):
-            kibana = Kibana("Kibana")
-            angular = Angular("web")
-            drone = Droneci("drone")
-            angular >> github >> drone >> dockerhub >> watchtower
-            airflow = Airflow("")
-            airflow >> logstash
-            elastic >> kibana
-        traefik = Traefik()
-        traefik >> logstash
+
+        kibana = Kibana("Kibana")
+        angular = Angular("Web app")
+        drone = Droneci("Drone")
+        jellyfin = Custom("Jellyfin", "./logos/jellyfin.png")
+        jupyter = Custom("", "./logos/jupyter.png")
+        concourse = Custom("Concourse", "./logos/concourse.png")
+        jenkins = Custom("", "./logos/jenkins.png")
+        mlflow = Custom("", "./logos/mlflow.png")
+        portainer = Custom("", "./logos/portainer.png")
+        angular >> github >> drone >> dockerhub >> watchtower
+        airflow = Airflow("Airflow")
+        airflow >> logstash
+        elastic >> kibana
+        watchtower >> angular
+        traefik = Traefik("Traefik reverse proxy")
         traefik >> angular
         traefik >> drone
         traefik >> kibana
         traefik >> airflow
+        traefik >> jellyfin
+        traefik >> jupyter
+        traefik >> concourse
+        traefik >> jenkins
+        traefik >> mlflow
+        traefik >> portainer
+
+        traefik_logs = Traefik("Traefik logs")
+        traefik_logs >> beats
+        beats >> elastic
+        with Cluster("Other"):
+            mumble = Custom("Mumble", "./logos/mumble.png")
+            factorio = Custom("Factorio", "./logos/factorio.png")
+
+            mumble - factorio
+
+    debian >> logstash
