663e753c28d99c418c086a1d3a6307d756e4fb2b
Alexander Dahl <alex@dahl.dev>
2022-12-08 19:35:46 +0100

    Add calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './about/about.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
