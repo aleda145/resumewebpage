c1baf71ae49dd040a85d6cd500eb1be959bd35af
Alexander Dahl <alex@dahl.dev>
2022-12-08 19:51:55 +0100

    Sloppy refactoring about and head to add cal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calendar', component: CalendarComponent},
+  {path: '', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
